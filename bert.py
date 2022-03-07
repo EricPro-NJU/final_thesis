db247b7a999e3ee1e9a511e62b03d9407826efd2
181840070 <geruipeng@smail.nju.edu.cn>
2022-03-07 11:50:15 +0800

    init

---
+++ bert.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertAdam
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class SimpleBert(nn.Module):
+    def __init__(self, seq_len, output_size):
+        super(SimpleBert, self).__init__()
+        self.seq_len = seq_len
+        self.output_size = output_size
+        self.d_model = 768
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        # 12-layer, 768-hidden, 12-heads, 110M parameters
+        # output: [batch_size, sequence_length, hidden_size]
+        self.linear = nn.Linear(self.d_model * self.seq_len, self.output_size)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, inputs):
+        '''
+        :param inputs: N * seq_len
+        :return: N * seq_len * output_size (after softmax, represent probability)
+        '''
+        bert_feature, _ = self.bert(inputs)
+        bert_output = bert_feature[11]
+        print(bert_output.shape)
+        batch_size = bert_feature[11].shape[0]
+        context = self.linear(bert_feature[11].view(batch_size, -1))
+        outputs = self.softmax(context)
+        return outputs
+
+
+def index_data(data_path, data_token_path=None, data_index_path=None, data_mask_path=None, data_label_path=None):
+    '''
+    Read data from file, and save tokens, indexes, mask if necessary
+    :param data_path:
+    :param data_token_path: if None, do not save to file
+    :param data_index_path:
+    :param data_mask_path:
+    :param data_label_path:
+    :return: 4 lists including data information
+    '''
+
+    data_list = []
+    token_list = []
+    index_list = []
+    mask_list = []
+    label_list = []
+
+    with open(data_path, "r", encoding="UTF-8-sig") as train_file:
+        linereader = train_file.readlines()
+        for line in linereader:
+            num = eval(line[0])
+            sentence = line[2:]
+            data_list.append([num, sentence.strip()])
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+    for item in data_list:
+        tokens = tokenizer.tokenize(item[1])
+        tokens_len = len(tokens)
+        if tokens_len > 510:
+            del tokens[128:-382]
+            tokens_len = 510
+        tokens.insert(0, "[CLS]")
+        tokens.append("[SEP]")
+        tokens_len += 2
+        pad_num = 512 - tokens_len
+        tokens.extend(["[PAD]"] * pad_num)
+        mask = [1] * tokens_len + [0] * pad_num
+        index = tokenizer.convert_tokens_to_ids(tokens)
+        token_list.append(tokens)
+        index_list.append(index)
+        mask_list.append(mask)
+        label_list.append(item[0])
+
+    if data_token_path is not None:
+        with open(data_token_path, "w", encoding="UTF-8") as fp:
+            for item in token_list:
+                fp.write("{}\n".format(item))
+
+    if data_index_path is not None:
+        with open(data_index_path, "w", encoding="UTF-8") as fp:
+            for item in index_list:
+                fp.write("{}\n".format(item))
+
+    if data_mask_path is not None:
+        with open(data_mask_path, "w", encoding="UTF-8") as fp:
+            for item in mask_list:
+                fp.write("{}\n".format(item))
+
+    if data_label_path is not None:
+        with open(data_label_path, "w", encoding="UTF-8") as fp:
+            for item in label_list:
+                fp.write("{}\n".format(item))
+
+    return token_list, index_list, mask_list, label_list
+
+
+def label_logits(labels, group_num):
+    logits = []
+    for item in labels:
+        prob = [0] * group_num
+        prob[item] = 1
+        logits.append(prob)
+    return logits
+
+
+class IMDBDataSet(Dataset):
+    def __init__(self, src_file, token_file=None, index_file=None, mask_file=None, label_file=None):
+        super(IMDBDataSet, self).__init__()
+        if src_file is not None:
+            token_list, index_list, mask_list, label_list = index_data(src_file, token_file, index_file, mask_file,
+                                                                       label_file)
+            self.input_idx = torch.LongTensor(index_list)  # num * seq_len
+            self.mask_idx = torch.LongTensor(mask_list)  # num * seq_len
+            self.label_idx = torch.LongTensor(label_logits(label_list, 2))  # num * 2
+        else:
+            if index_file is None or mask_file is None or label_file is None:
+                raise FileNotFoundError("You should identify source file of data.")
+            index = []
+            mask = []
+            label = []
+            with open(index_file, "r", encoding="UTF-8") as fp:
+                linereader = fp.readlines()
+                for line in linereader:
+                    index.append(eval(line))
+            with open(mask_file, "r", encoding="UTF-8") as fp:
+                linereader = fp.readlines()
+                for line in linereader:
+                    mask.append(eval(line))
+            with open(label_file, "r", encoding="UTF-8") as fp:
+                linereader = fp.readlines()
+                for line in linereader:
+                    label.append(eval(line))
+            self.input_idx = torch.LongTensor(index)
+            self.mask_idx = torch.LongTensor(mask)
+            self.label_idx = torch.LongTensor(label_logits(label, 2))
+
+    def __len__(self):
+        return self.input_idx.shape[0]
+
+    def __getitem__(self, idx):
+        return self.input_idx[idx], self.mask_idx[idx], self.label_idx[idx]
+
+
+def train():
+    print("Indexing Training Data......")
+    train_file = "/content/drive/MyDrive/bert/IMDBtrain.csv"
+    train_token_file = "/content/drive/MyDrive/bert/IMDBtrain_token.txt"
+    train_index_file = "/content/drive/MyDrive/bert/IMDBtrain_index.txt"
+    train_mask_file = "/content/drive/MyDrive/bert/IMDBtrain_mask.txt"
+    train_label_file = "/content/drive/MyDrive/bert/IMDBtrain_label.txt"
+    trainloader = DataLoader(IMDBDataSet(None, train_token_file, train_index_file, train_mask_file, train_label_file),
+                             batch_size=32, shuffle=True)
+    print("Index Training Data Done.")
+    '''
+    print("Indexing Testing Data......")
+    test_file = "IMDBtest.csv"
+    testloader = DataLoader(IMDBDataSet(test_file), batch_size=32, shuffle=True)
+    print("Index Testing Data Done.")
+    '''
+
+    print("Model Config......")
+    model = SimpleBert(512, 2).to(device)
+    model.train()
+
+    epoch_num = 4
+    lr = 2e-5
+    warmup = 0.1
+    t_total = 1e5
+
+    criterion = nn.CrossEntropyLoss(ignore_index=0)
+    optimizer = BertAdam(model.parameters(), lr=lr, warmup=warmup, t_total=t_total)
+    print("Model Config Done.")
+
+    print("Start Training.")
+    print("WARNING: This is only a demo of reading dataloader!")
+    remaining = 2
+    for inputs, mask, label in trainloader:
+        remaining -= 1
+        inputs.to(device)
+        mask.to(device)
+        label.to(device)
+        if remaining >= 0:
+            print("inputs shape: {}\nmask shape: {}\nlabel shape:{}".format(inputs.shape, mask.shape, label.shape))
+            print("inputs: {}\n mask: {}\n label: {}".format(inputs, mask, label))
+
+    print("Training Done.")
+
+
+if __name__ == "__main__":
+    train()
---
+++ transformer.py
@@ -0,0 +1,334 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import math
+import torch.utils.data as Data
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def getPadMask(seq_q, seq_k, pad):
+    """
+        :param seq_q: N * len_q
+        :param seq_k: N * len_k
+        :param pad: the index of [PAD]
+        :return: mask: N * len_q * len_k
+        """
+    batch_size, len_q = seq_q.size()
+    _, len_k = seq_k.size()
+    mask = torch.zeros([batch_size, len_q, len_k])
+    matrix_q = seq_q.unsqueeze(1).transpose(-1, -2).repeat(1, 1, len_k)
+    matrix_k = seq_k.unsqueeze(1).repeat(1, len_q, 1)
+    mask[matrix_q == pad] = 1
+    mask[matrix_k == pad] = 1
+    return mask
+
+
+def getDecoderMask(seq):
+    """
+    :param seq: tgt sequence: N * tgt_len
+    :return: mask: N * tgt_len * tgt_len
+    """
+    batch_size, tgt_len = seq.size()
+    mask = torch.zeros(batch_size, tgt_len, tgt_len)
+    row_idx = torch.arange(0, tgt_len, 1).unsqueeze(0).unsqueeze(0).transpose(-1, -2).repeat(batch_size, 1, tgt_len)
+    col_idx = torch.arange(0, tgt_len, 1).unsqueeze(0).unsqueeze(0).repeat(batch_size, tgt_len, 1)
+    mask[row_idx < col_idx] = 1
+    mask[row_idx >= col_idx] = 0
+    return mask
+
+
+def getPositionalEmbedding(batch_size, seq_len, d_model):
+    """
+        :param seq_len:  length of input sequence
+        :param d_model:  dimension of embedding
+        :return: pos_emb : N * seq_len * d_model
+        """
+    pos_emb = torch.zeros(batch_size, seq_len, d_model)
+    pos_idx = torch.arange(0, seq_len).unsqueeze(0).unsqueeze(-1).repeat(batch_size, 1, d_model)
+    pow_idx = torch.arange(0, d_model)
+    pow_idx[pow_idx % 2 == 1] -= 1
+    pow_idx = pow_idx.unsqueeze(0).unsqueeze(0).repeat(batch_size, seq_len, 1)
+    pos_emb = pos_idx / torch.pow(10000, pow_idx / d_model)
+    pos_emb[:, :, 0::2] = torch.sin(pos_emb[:, :, 0::2])
+    pos_emb[:, :, 1::2] = torch.cos(pos_emb[:, :, 1::2])
+    return pos_emb
+
+class Configuration:
+    def __init__(self):
+        self.d_model = 512  # arbitrary
+        self.src_len = 5
+        self.tgt_len = 6
+        self.src_vocab_size = 10
+        self.tgt_vocab_size = 10
+        self.batch_size = 1  # arbitrary
+        self.dimq = 64  # arbitrary
+        self.dimv = 64  # arbitrary
+        self.n_heads = 8  # arbitrary
+        self.d_hidden = 2048  # arbitrary
+        self.n_layer = 6  # arbitrary
+        self.epoch_num = 10  # arbitrary
+        self.code_dict = {"pad": 0, "start": 1, "end": 2}
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, d_model, dimq, dimv, n_heads):
+        super(MultiheadAttention, self).__init__()
+        self.d_model = d_model
+        self.dimq = dimq
+        self.dimk = dimq
+        self.dimv = dimv
+        self.WQ = nn.Linear(in_features=d_model, out_features=dimq * n_heads, bias=False)
+        self.WK = nn.Linear(in_features=d_model, out_features=dimq * n_heads, bias=False)
+        self.WV = nn.Linear(in_features=d_model, out_features=dimv * n_heads, bias=False)
+        self.linear = nn.Linear(in_features=dimv * n_heads, out_features=d_model, bias=False)
+        self.layernorm = nn.LayerNorm(d_model)
+
+    def forward(self, inputQ, inputK, inputV, mask):
+        """
+        :param inputQ: N * seq_len * d_model (seq_len could be src_len or tgt_len)
+        :param inputK: N * seq_len * d_model
+        :param inputV: N * seq_len * d_model
+        :param mask: N * seq_len * seq_len
+        :return: output: N * seq_len * d_model
+        """
+        Q = self.WQ(inputQ)  # N * seq_len * (n_heads*dim_q)
+        K = self.WK(inputK)  # N * seq_len * (n_heads*dim_k)
+        V = self.WV(inputV)  # N * seq_len * (n_heads*dim_v)
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / torch.sqrt(torch.tensor([self.dimk]).cuda())  # N * seq_len * seq_len
+        scores[mask == 1] = -1e9
+        weights = torch.matmul(F.softmax(scores, dim=-1), V)  # N * seq_len * (n_heads*dim_v)
+        output = self.layernorm(self.linear(weights) + inputQ)
+        return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_hidden):
+        super(FeedForward, self).__init__()
+        self.d_model = d_model
+        self.d_hidden = d_hidden
+        self.linear1 = nn.Linear(in_features=d_model, out_features=d_hidden)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(in_features=d_hidden, out_features=d_model)
+        self.layernorm = nn.LayerNorm(d_model)
+
+    def forward(self, inputs):
+        """
+        :param inputs: N * seq_len * d_model (seq_len could be src_len or tgt_len)
+        :return: output: N * seq_len * d_model
+        """
+        context = self.linear1(inputs)
+        context = self.relu(context)
+        context = self.linear2(context)
+        output = self.layernorm(inputs + context)
+        return output
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, conf):
+        super(EncoderLayer, self).__init__()
+        self.multiheadSelfAttention = MultiheadAttention(conf.d_model, conf.dimq, conf.dimv, conf.n_heads)
+        self.feedForward = FeedForward(conf.d_model, conf.d_hidden)
+
+    def forward(self, inputs, mask):
+        """
+        :param inputs: N * src_len * d_model
+        :param mask: N * src_len * src_len
+        :return: output: N * src_len * d_model
+        """
+        attn = self.multiheadSelfAttention(inputs, inputs, inputs, mask)
+        output = self.feedForward(attn)
+        return output
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, conf):
+        super(TransformerEncoder, self).__init__()
+        self.conf = conf
+        self.word_emb = nn.Embedding(conf.src_vocab_size, conf.d_model)
+        self.layers = nn.ModuleList([EncoderLayer(conf) for _ in range(conf.n_layer)])
+
+    def forward(self, input_seq, pad_mask):
+        """
+        :param input_seq:  N * src_len
+        :return: N * src_len * d_model
+        """
+        wemb = self.word_emb(input_seq)
+        pemb = getPositionalEmbedding(self.conf.batch_size, self.conf.src_len, self.conf.d_model).cuda()
+        encoder_output = wemb + pemb
+        # encoder_multiselfattn_mask = getPadMask(input_seq, input_seq, self.conf.code_dict["pad"])
+        for layer in self.layers:
+            encoder_output = layer(encoder_output, pad_mask)
+
+        return encoder_output
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, conf):
+        super(DecoderLayer, self).__init__()
+        self.conf = conf
+        self.multiheadSelfAttention = MultiheadAttention(conf.d_model, conf.dimq, conf.dimv, conf.n_heads)
+        self.multiheadAttention = MultiheadAttention(conf.d_model, conf.dimq, conf.dimv, conf.n_heads)
+        self.feedForward = FeedForward(conf.d_model, conf.d_hidden)
+
+    def forward(self, inputs, encoder_output, self_mask, cross_mask):
+        """
+        :param encoder_output: N * src_len * d_model
+        :param inputs:  N * tgt_len * d_model
+        :param self_mask:  N * tgt_len * tgt_len
+        :param cross_mask:  N * tgt_len * seq_len
+        :return:  N * tgt_len * d_model
+        """
+        attn1 = self.multiheadSelfAttention(inputs, inputs, inputs, self_mask)
+        attn2 = self.multiheadAttention(attn1, encoder_output, encoder_output, cross_mask)
+        output = self.feedForward(attn2)
+        return output
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, conf):
+        super(TransformerDecoder, self).__init__()
+        self.conf = conf
+        self.word_emb = nn.Embedding(conf.tgt_vocab_size, conf.d_model)
+        self.layers = nn.ModuleList([DecoderLayer(conf) for _ in range(conf.n_layer)])
+
+    def forward(self, input_seq, encoder_output, self_mask, cross_mask):
+        """
+        :param input_seq: N * tgt_len
+        :param encoder_output:  N * src_len * d_model
+        :return:  N * tgt_len * d_model
+        """
+        wemb = self.word_emb(input_seq)
+        pemb = getPositionalEmbedding(self.conf.batch_size, self.conf.tgt_len, self.conf.d_model).cuda()
+        # pad_mask = getPadMask(input_seq, input_seq, self.conf.code_dict["pad"])
+        # dec_mask = getDecoderMask(input_seq)
+        # cross_mask = getPadMask(input_seq, encoder_output, self.conf.code_dict["pad"])
+        decoder_output = wemb + pemb
+        for layer in self.layers:
+            decoder_output = layer(decoder_output, encoder_output, self_mask, cross_mask)
+
+        return decoder_output
+
+class Transformer(nn.Module):
+    def __init__(self, conf):
+        super(Transformer, self).__init__()
+        self.conf = conf
+        self.encoder = TransformerEncoder(conf)
+        self.decoder = TransformerDecoder(conf)
+        self.linear = nn.Linear(conf.d_model, conf.tgt_vocab_size)
+
+    def forward(self, encoder_input, decoder_input):
+        """
+        :param encoder_input: N * src_len
+        :param decoder_input: N * tgt_len
+        :return: N * tgt_len
+        """
+        encoder_mask = getPadMask(encoder_input, encoder_input, self.conf.code_dict["pad"])
+        decoder_mask = getPadMask(decoder_input, decoder_input, self.conf.code_dict["pad"]) + getDecoderMask(
+            decoder_input)
+        cross_mask = getPadMask(decoder_input, encoder_input, self.conf.code_dict["pad"])
+        encoder_output = self.encoder(encoder_input, encoder_mask)
+        decoder_output = self.decoder(decoder_input, encoder_output, decoder_mask, cross_mask)
+        prob_matrix = F.softmax(self.linear(decoder_output), dim=-1) # N * tgt_len * tgt_vocab_size
+        output_seq = torch.argmax(prob_matrix, dim=-1)
+        return output_seq, prob_matrix
+
+'''
+if __name__ == "__main__":
+    conf = Configuration()
+
+    model = Transformer(conf).cuda()
+
+    print(model)
+
+    src = torch.tensor([[3, 4, 5, 6, 7], [3, 5, 4, 7, 0]]).cuda()
+
+    tgt = torch.tensor([[4, 2, 5, 3, 0, 0], [9, 5, 7, 4, 3, 5]]).cuda()
+
+    output = model(src, tgt)
+    print(src.size(), src.dtype)
+    print(tgt.size(), tgt.dtype)
+    print(output.size(), output.dtype)
+'''
+
+conf = Configuration()
+
+sentences = [
+    # enc_input           dec_input         dec_output
+    ['ich mochte ein bier P', 'S i want a beer .', 'i want a beer . E'],
+    ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']
+]
+
+# Padding Should be Zero
+src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4, 'cola': 5}
+conf.src_vocab_size = len(src_vocab)
+
+tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'coke': 5, 'S': 6, 'E': 7, '.': 8}
+idx2word = {i: w for i, w in enumerate(tgt_vocab)}
+conf.tgt_vocab_size = len(tgt_vocab)
+
+conf.src_len = 5  # enc_input max sequence length
+conf.tgt_len = 6  # dec_input(=dec_output) max sequence length
+
+
+def make_data(sentences):
+    enc_inputs, dec_inputs, dec_outputs = [], [], []
+    for i in range(len(sentences)):
+        enc_input = [[src_vocab[n] for n in sentences[i][0].split()]]  # [[1, 2, 3, 4, 0], [1, 2, 3, 5, 0]]
+        dec_input = [[tgt_vocab[n] for n in sentences[i][1].split()]]  # [[6, 1, 2, 3, 4, 8], [6, 1, 2, 3, 5, 8]]
+        dec_output = [[tgt_vocab[n] for n in sentences[i][2].split()]]  # [[1, 2, 3, 4, 8, 7], [1, 2, 3, 5, 8, 7]]
+
+        enc_inputs.extend(enc_input)
+        dec_inputs.extend(dec_input)
+        dec_outputs.extend(dec_output)
+
+    return torch.LongTensor(enc_inputs), torch.LongTensor(dec_inputs), torch.LongTensor(dec_outputs)
+
+
+enc_inputs, dec_inputs, dec_outputs = make_data(sentences)
+
+
+class MyDataSet(Data.Dataset):
+    def __init__(self, enc_inputs, dec_inputs, dec_outputs):
+        super(MyDataSet, self).__init__()
+        self.enc_inputs = enc_inputs
+        self.dec_inputs = dec_inputs
+        self.dec_outputs = dec_outputs
+
+    def __len__(self):
+        return self.enc_inputs.shape[0]
+
+    def __getitem__(self, idx):
+        return self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
+
+
+loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), 2, True)
+
+conf.code_dict["start"] = 6
+conf.code_dict["end"] = 7
+
+
+model = Transformer(conf).cuda()
+criterion = nn.CrossEntropyLoss(ignore_index=conf.code_dict["pad"])
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.99)
+for epoch in range(30):
+    for enc_inputs, dec_inputs, dec_outputs in loader:
+      '''
+      enc_inputs: [batch_size, src_len]
+      dec_inputs: [batch_size, tgt_len]
+      dec_outputs: [batch_size, tgt_len]
+      '''
+      enc_inputs, dec_inputs, dec_outputs = enc_inputs.cuda(), dec_inputs.cuda(), dec_outputs.cuda()
+      # outputs: [batch_size * tgt_len, tgt_vocab_size]
+      outputs, prob_matrix = model(enc_inputs, dec_inputs)
+      loss = criterion(prob_matrix.view(prob_matrix.shape[0]*prob_matrix.shape[1], prob_matrix.shape[2]), dec_outputs.view(-1))
+      print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+
+      optimizer.zero_grad()
+      loss.backward()
+
+
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print(name)
