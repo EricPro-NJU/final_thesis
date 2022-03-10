258aff031a92ba9225fbb45b7b359970f7347a8a
181840070 <geruipeng@smail.nju.edu.cn>
2022-03-10 16:50:36 +0800

    minor fixed

--- transformer.py
+++ transformer.py
@@ -251,84 +251,84 @@ if __name__ == "__main__":
     print(tgt.size(), tgt.dtype)
     print(output.size(), output.dtype)
 '''
+if __name__ == "__main__":
 
-conf = Configuration()
+    conf = Configuration()
 
-sentences = [
-    # enc_input           dec_input         dec_output
-    ['ich mochte ein bier P', 'S i want a beer .', 'i want a beer . E'],
-    ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']
-]
+    sentences = [
+        # enc_input           dec_input         dec_output
+        ['ich mochte ein bier P', 'S i want a beer .', 'i want a beer . E'],
+        ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']
+    ]
 
-# Padding Should be Zero
-src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4, 'cola': 5}
-conf.src_vocab_size = len(src_vocab)
+    # Padding Should be Zero
+    src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4, 'cola': 5}
+    conf.src_vocab_size = len(src_vocab)
 
-tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'coke': 5, 'S': 6, 'E': 7, '.': 8}
-idx2word = {i: w for i, w in enumerate(tgt_vocab)}
-conf.tgt_vocab_size = len(tgt_vocab)
+    tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'coke': 5, 'S': 6, 'E': 7, '.': 8}
+    idx2word = {i: w for i, w in enumerate(tgt_vocab)}
+    conf.tgt_vocab_size = len(tgt_vocab)
 
-conf.src_len = 5  # enc_input max sequence length
-conf.tgt_len = 6  # dec_input(=dec_output) max sequence length
-
-
-def make_data(sentences):
-    enc_inputs, dec_inputs, dec_outputs = [], [], []
-    for i in range(len(sentences)):
-        enc_input = [[src_vocab[n] for n in sentences[i][0].split()]]  # [[1, 2, 3, 4, 0], [1, 2, 3, 5, 0]]
-        dec_input = [[tgt_vocab[n] for n in sentences[i][1].split()]]  # [[6, 1, 2, 3, 4, 8], [6, 1, 2, 3, 5, 8]]
-        dec_output = [[tgt_vocab[n] for n in sentences[i][2].split()]]  # [[1, 2, 3, 4, 8, 7], [1, 2, 3, 5, 8, 7]]
-
-        enc_inputs.extend(enc_input)
-        dec_inputs.extend(dec_input)
-        dec_outputs.extend(dec_output)
-
-    return torch.LongTensor(enc_inputs), torch.LongTensor(dec_inputs), torch.LongTensor(dec_outputs)
+    conf.src_len = 5  # enc_input max sequence length
+    conf.tgt_len = 6  # dec_input(=dec_output) max sequence length
 
 
-enc_inputs, dec_inputs, dec_outputs = make_data(sentences)
+    def make_data(sentences):
+        enc_inputs, dec_inputs, dec_outputs = [], [], []
+        for i in range(len(sentences)):
+            enc_input = [[src_vocab[n] for n in sentences[i][0].split()]]  # [[1, 2, 3, 4, 0], [1, 2, 3, 5, 0]]
+            dec_input = [[tgt_vocab[n] for n in sentences[i][1].split()]]  # [[6, 1, 2, 3, 4, 8], [6, 1, 2, 3, 5, 8]]
+            dec_output = [[tgt_vocab[n] for n in sentences[i][2].split()]]  # [[1, 2, 3, 4, 8, 7], [1, 2, 3, 5, 8, 7]]
+
+            enc_inputs.extend(enc_input)
+            dec_inputs.extend(dec_input)
+            dec_outputs.extend(dec_output)
+
+        return torch.LongTensor(enc_inputs), torch.LongTensor(dec_inputs), torch.LongTensor(dec_outputs)
 
 
-class MyDataSet(Data.Dataset):
-    def __init__(self, enc_inputs, dec_inputs, dec_outputs):
-        super(MyDataSet, self).__init__()
-        self.enc_inputs = enc_inputs
-        self.dec_inputs = dec_inputs
-        self.dec_outputs = dec_outputs
-
-    def __len__(self):
-        return self.enc_inputs.shape[0]
-
-    def __getitem__(self, idx):
-        return self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
+    enc_inputs, dec_inputs, dec_outputs = make_data(sentences)
 
 
-loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), 2, True)
+    class MyDataSet(Data.Dataset):
+        def __init__(self, enc_inputs, dec_inputs, dec_outputs):
+            super(MyDataSet, self).__init__()
+            self.enc_inputs = enc_inputs
+            self.dec_inputs = dec_inputs
+            self.dec_outputs = dec_outputs
 
-conf.code_dict["start"] = 6
-conf.code_dict["end"] = 7
+        def __len__(self):
+            return self.enc_inputs.shape[0]
 
-
-model = Transformer(conf).cuda()
-criterion = nn.CrossEntropyLoss(ignore_index=conf.code_dict["pad"])
-optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.99)
-for epoch in range(30):
-    for enc_inputs, dec_inputs, dec_outputs in loader:
-      '''
-      enc_inputs: [batch_size, src_len]
-      dec_inputs: [batch_size, tgt_len]
-      dec_outputs: [batch_size, tgt_len]
-      '''
-      enc_inputs, dec_inputs, dec_outputs = enc_inputs.cuda(), dec_inputs.cuda(), dec_outputs.cuda()
-      # outputs: [batch_size * tgt_len, tgt_vocab_size]
-      outputs, prob_matrix = model(enc_inputs, dec_inputs)
-      loss = criterion(prob_matrix.view(prob_matrix.shape[0]*prob_matrix.shape[1], prob_matrix.shape[2]), dec_outputs.view(-1))
-      print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
-
-      optimizer.zero_grad()
-      loss.backward()
+        def __getitem__(self, idx):
+            return self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
 
 
-for name, param in model.named_parameters():
-    if param.requires_grad:
-        print(name)
+    loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), 2, True)
+
+    conf.code_dict["start"] = 6
+    conf.code_dict["end"] = 7
+
+    model = Transformer(conf).cuda()
+    criterion = nn.CrossEntropyLoss(ignore_index=conf.code_dict["pad"])
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.99)
+    for epoch in range(30):
+        for enc_inputs, dec_inputs, dec_outputs in loader:
+            '''
+            enc_inputs: [batch_size, src_len]
+            dec_inputs: [batch_size, tgt_len]
+            dec_outputs: [batch_size, tgt_len]
+            '''
+            enc_inputs, dec_inputs, dec_outputs = enc_inputs.cuda(), dec_inputs.cuda(), dec_outputs.cuda()
+            # outputs: [batch_size * tgt_len, tgt_vocab_size]
+            outputs, prob_matrix = model(enc_inputs, dec_inputs)
+            loss = criterion(prob_matrix.view(prob_matrix.shape[0] * prob_matrix.shape[1], prob_matrix.shape[2]),
+                             dec_outputs.view(-1))
+            print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+
+            optimizer.zero_grad()
+            loss.backward()
+
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(name)
