a17428405f2288c3a1094e24b7398b944b698e1b
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-03 21:39:29 +0800

    change model architecture

--- command.sh
+++ command.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-python running.py -n IMDB_BRNN_REV2_FIT -d IMDB -m bert_lstm2 -ev --read_from_cache --test_model_path /root/autodl-nas/checkpoint/IMDB_BRNN_REV2_FIT.pb --alarm
-python running.py -n IMDB_BRNN_REV1_FPT100kFIT -d IMDB -m bert_lstm -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache --alarm
-python running.py -n IMDB_BRNN_REV2_FPT100kFIT -d IMDB -m bert_lstm2 -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache --alarm
+python running.py -n IMDB_BRNN_REV3_FIT -d IMDB -m bert_lstm3 -fit -ev --read_from_cache
+python running.py -n IMDB_BRNN_REV3_FPT100kFIT -d IMDB -m bert_lstm3 -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache
 shutdown
