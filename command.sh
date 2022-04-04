8e292dc0fde076efd6b2b4dc0dc24f5217f37586
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-04 17:43:14 +0800

    aaa

--- command.sh
+++ command.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python running.py -n IMDB_BRNN_final_FIT -d IMDB -m bert_lstm -fit -ev --read_from_cache
+python running.py -n IMDB_BRNN_addnorm_FIT -d IMDB -m bert_lstm3 -fit -ev --read_from_cache
 # python running.py -n IMDB_BRNN_final_FPT100kFIT -d IMDB -m bert_lstm -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache
 shutdown
