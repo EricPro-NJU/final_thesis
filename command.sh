81505a0120df3240a1552a889f20f87afea21e3b
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-04 17:34:25 +0800

    change of scripts

--- command.sh
+++ command.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python running.py -n IMDB_BRNN_addall_FIT -d IMDB -m bert_lstm1 -fit -ev --read_from_cache
-python running.py -n IMDB_BRNN_addall_FPT100kFIT -d IMDB -m bert_lstm1 -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache
+python running.py -n IMDB_BRNN_final_FIT -d IMDB -m bert_lstm -fit -ev --read_from_cache
+# python running.py -n IMDB_BRNN_final_FPT100kFIT -d IMDB -m bert_lstm -fit -ev --fit_ftp_path /root/autodl-nas/checkpoint/IMDB_FtP.pb --read_from_cache
 shutdown
