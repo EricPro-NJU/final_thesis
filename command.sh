e9f6f855e0f98e6b67f8f9510adadec0e8c3bbf7
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-04 22:18:36 +0800

    change of scripts

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python running.py -n Sogou_BLN_fit -d Sogou -m bert_linear -fit -ev --read_from_cache --fit_batch_size 20
-python running.py -n Sogou_BRNN_fit -d Sogou -m bert_lstm -fit -ev --read_from_cache
+python running.py -n Sogou -d Sogou -m bert_linear -ftp --read_from_cache 
+
 
 shutdown
