93ee0aac6965e97859a11d6f545e125b176f8481
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-05 15:54:59 +0800

    change of scripts

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m textrnn -tr -ev --read_from_cache
-python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m textcnn -tr -ev --read_from_cache
-python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m transformer -tr -ev --read_from_cache
+python running.py -n Sogou_textrnn -d Sogou -m textrnn -tr -ev --read_from_cache
+python running.py -n Sogou_textcnn -d Sogou -m textcnn -tr -ev --read_from_cache
+python running.py -n Sogou_trans -d Sogou -m transformer -tr -ev --read_from_cache
 
 shutdown
