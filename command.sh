e00cd2024cfc2e300cc09c7b7a8f72608e1e2f20
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-05 09:46:59 +0800

    change of scripts

--- command.sh
+++ command.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-python running.py -n Sogou -d Sogou -m bert_linear -ftp --read_from_cache 
+python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m bert_linear -fit -ev --read_from_cache --fit_batch_size 20 --fit_ftp_path /root/autodl-nas/checkpoint/Sogou_FtP.pb --alarm
 
+python running.py -n Sogou_BRNN_FTP100kFIT -d Sogou -m bert_lstm -fit -ev --read_from_cache --fit_batch_size 16 --fit_ftp_path /root/autodl-nas/checkpoint/Sogou_FtP.pb --alarm
 
 shutdown
