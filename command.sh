06ccf467feb7237f5ba5385e7a90b3b38a3ec3f3
181840070 <geruipeng@smail.nju.edu.cn>
2022-04-05 09:50:00 +0800

    aaa

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m bert_linear -fit -ev --read_from_cache --fit_batch_size 20 --fit_ftp_path /root/autodl-nas/checkpoint/Sogou_FtP.pb --alarm
+python running.py -n Sogou_BLN_FTP100kFIT -d Sogou -m bert_linear -fit -ev --read_from_cache --fit_batch_size 16 --fit_ftp_path /root/autodl-nas/checkpoint/Sogou_FtP.pb --alarm
 
 python running.py -n Sogou_BRNN_FTP100kFIT -d Sogou -m bert_lstm -fit -ev --read_from_cache --fit_batch_size 16 --fit_ftp_path /root/autodl-nas/checkpoint/Sogou_FtP.pb --alarm
 
